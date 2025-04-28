004e2ede8bb7dedf2ee16a3042dd81acb86dbb4c
Simon Bédard <JavaKhanStudio@Gmail.com>
2025-04-28 14:49:27 +0200

    Init Commit

---
+++ src/test/java/com/jks/deploy/renderDeploy/RenderDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jks.deploy.renderDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
